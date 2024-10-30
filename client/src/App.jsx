2c8753f57f139caa0909e7789ae7937cb18400c8
Brandon Ngo <brandonngo56@gmail.com>
2024-10-29 22:31:20 -0400

    Ticket 1 Sam
    Created Navigation bar, Home Page, Recommended Page with 4 pictures of the exercises which would route them each page where it list exercises

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,34 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
 
-const App = () => {
+import { HashRouter as Router, Route,Routes } from 'react-router-dom';
+import Home from './Home';
+import Navbar from './Navbar';
+import Recommended from './Recommended';
+import LegPage from './LegPage';
+import ChestgPage from './ChestPage';
+import ArmPage from './ArmPage';
+import BackPage from './BackPage';
+
+
+function App() {
+
+
   return (
-    <div className="w-full p-6">
-      <Navbar />
-      <Outlet />
+    <div>
+      <Router>
+      <div className='Route'>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/recommended" element={<Recommended/>}/>
+          <Route path='/leg' element={<LegPage/>}/>
+          <Route path='/chest' element={<ChestgPage/>}/>
+          <Route path='/arm' element={<ArmPage/>}/>
+          <Route path='/back' element={<BackPage/>}/>
+        </Routes>
+      </div>
+      </Router>
     </div>
   );
-};
-export default App
+}
+
+export default App;
