d5bc8c132d3599c3a0face81ded9cb0ccca345fb
davit3331 <129024554+davit3331@users.noreply.github.com>
2024-11-01 16:18:07 -0400

    Add files via upload

---
+++ components/Home.jsx
@@ -0,0 +1,11 @@
+import FitGenius from '../pictures/FitGenius.png'
+
+function Home(){
+
+    return(
+        <div>
+            <h1 className='AppName'>Fit Genius</h1>
+        </div>
+    )
+}
+export default Home;
