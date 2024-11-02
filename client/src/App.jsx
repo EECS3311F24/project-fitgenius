cfbf388da063f4a05742cc0acad9449753d53b93
Ayaan <mayaan0606@gmail.com>
2024-11-01 20:17:42 -0400

    Added GIFs for chest excercises and leg excercises

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,52 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Navbar from './Navbar';
-import Recommended from './Recommended';
-import LegPage from './LegPage';
-import ChestgPage from './ChestPage';
-import ArmPage from './ArmPage';
-import BackPage from './BackPage';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Recommended from "./Recommended";
+import LegPage from "./LegPage";
+import ChestgPage from "./ChestPage";
+import ArmPage from "./ArmPage";
+import BackPage from "./BackPage";
+import BenchPress from "./Chest Stuff/BenchPress";
+import CableCrossOver from "./Chest Stuff/CableCrossOver";
+import ParallelBarDip from "./Chest Stuff/ParallelBarDip";
+import PushUps from "./Chest Stuff/PushUps";
+import PlankRotations from "./Chest Stuff/PlankRotations";
+import LegPress from "./Leg Stuff/LegPress";
+import BulgarianSplitSquat from "./Leg Stuff/BulgarianSplitSquat";
+import FrontSquat from "./Leg Stuff/FrontSquat";
+import LegExtensions from "./Leg Stuff/LegExtensions";
 
 function App() {
   return (
     <div>
       <Router>
-      <div className='Route'>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/recommended" element={<Recommended/>}/>
-          <Route path='/leg' element={<LegPage/>}/>
-          <Route path='/chest' element={<ChestgPage/>}/>
-          <Route path='/arm' element={<ArmPage/>}/>
-          <Route path='/back' element={<BackPage/>}/>
-        </Routes>
-      </div>
+        <div className="Route">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recommended" element={<Recommended />} />
+            <Route path="/leg" element={<LegPage />} />
+            <Route path="/chest" element={<ChestgPage />} />
+            <Route path="/arm" element={<ArmPage />} />
+            <Route path="/back" element={<BackPage />} />
+
+            <Route path="/bench-press" element={<BenchPress />} />
+            <Route path="/cable-crossover" element={<CableCrossOver />} />
+            <Route path="/parallel-bar-dip" element={<ParallelBarDip />} />
+            <Route path="/push-ups" element={<PushUps />} />
+            <Route path="/plank-rotations" element={<PlankRotations />} />
+
+            <Route path="/leg-press" element={<LegPress />} />
+            <Route
+              path="/bulgarian-split-squat"
+              element={<BulgarianSplitSquat />}
+            />
+
+            <Route path="/front-squat" element={<FrontSquat />} />
+
+            <Route path="/leg-extensions" element={<LegExtensions />} />
+          </Routes>
+        </div>
       </Router>
     </div>
   );
---
+++ client/src/Chest Stuff/PlankRotations.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import benchPressGif from "../assets/fa4932a8865f085e6240101fa9a11447.gif"; // Adjust the path if needed
+
+const PlankRotations = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        backgroundColor: "#b3cde0",
+        position: "relative",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "3rem", // Larger font size for heading
+          position: "absolute",
+          top: "20px", // Place heading at the top
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        Plank Rotations
+      </h1>
+      <img
+        src={benchPressGif}
+        alt="CC Exercise"
+        style={{
+          width: "40%", // Sets the GIF to 50% of the container's width; increase this as needed
+          maxWidth: "500px", // Set a max width to control the size on larger screens
+          height: "auto", // Maintain aspect ratio
+        }}
+      />
+    </div>
+  );
+};
+
+export default PlankRotations;
